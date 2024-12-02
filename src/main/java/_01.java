cf13b1b573d0b3aa64707bfac1df5d560d35792f
Martin VÁŇA <martin.vana@tipsport.cz>
2024-12-02 10:39:14 +0100

    Day 1

--- src/main/java/_01.java
+++ src/main/java/_01.java
@@ -1,13 +1,47 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
 
 void main() throws Exception {
+    // 1. part
+    List<Integer> listA = new ArrayList<>();
+    List<Integer> listB = new ArrayList<>();
+
     // Load the resource file
     try (InputStream inputStream = getClass().getResourceAsStream("01.txt");
          BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-        // Read and print the file line by line
-        String line;
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
-        }
+        reader.lines().forEach((line) -> {
+            List<Integer> numbers = parseNumbersFromLine(line);
+            listA.add(numbers.get(0));
+            listB.add(numbers.get(1));
+        });
     }
+
+    listA.sort(Integer::compareTo);
+    listB.sort(Integer::compareTo);
+
+    int totalDistance = 0;
+    for (int i = 0; i < listA.size(); i++) {
+        totalDistance += Math.abs(listA.get(i) - listB.get(i));
+    }
+    System.out.println(totalDistance);
+
+    // 2. part
+    Map<Integer, Integer> counterB = new HashMap<>();
+    for (Integer integer : listB) {
+        counterB.put(integer, counterB.getOrDefault(integer, 0) + 1);
+    }
+    int similarityScore = 0;
+    for (Integer integer : listA) {
+        similarityScore += integer * counterB.getOrDefault(integer, 0);
+    }
+    System.out.println(similarityScore);
+}
+
+List<Integer> parseNumbersFromLine(String line) {
+    return Arrays.stream(line.split("\s+"))
+            .mapToInt(Integer::parseInt)
+            .boxed()
+            .toList();
 }
