aefed4c21e7c4a0f4d5afbb08a4b0e8945978e0d
Martin VÁŇA <martin.vana@tipsport.cz>
2024-12-17 20:25:11 +0100

    Day 17 - generic solution

--- src/main/java/_17.java
+++ src/main/java/_17.java
@@ -79,65 +79,144 @@ void main() throws Exception {
         System.out.println(output.stream()
                 .map(Object::toString)
                 .collect(Collectors.joining(",")));
+
+        // 2. part - Hardcoded input algorithm :(
+        List<Integer> expectedOutput = List.of(2,4,1,6,7,5,4,6,1,4,5,5,0,3,3,0);
+        long initialRegisterA = solvePart2Hardcoded(expectedOutput, 0, expectedOutput.size() - 1);
+
+        A = initialRegisterA;
+        output = new ArrayList<>();
+        while (A != 0) {
+    //        // 2,4,
+    //        B = A % 8;
+    //        // 1,6,
+    //        B ^= 6;
+    //        // 7,5,
+    //        C = A / powerN(2, B);
+    //        // 4,6,
+    //        B ^= C;
+    //        // 1,4,
+    //        B ^= 4;
+    //        // 5,5,
+    //        output.add(String.valueOf(B % 8));
+    //        // 0,3,
+    //        A /= 8;
+    //        // 3,0
+
+            // 2,4,1,6,7,5,4,6,1,4,
+            long o = (((A % 8) ^ 6) ^ (A / powerN(2, (A % 8) ^ 6)) ^ 4) % 8;
+
+            // 5,5,
+            output.add(o);
+            // 0,3,
+            A /= 8;
+            // 3,0
+        }
+
+        System.out.println(expectedOutput.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(",")));
+        System.out.println(output.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(",")));
+        System.out.println(initialRegisterA);
+
+        // 2. part - Generic algorithm :)
+        initialRegisterA = solvePart2(program, program, 0, program.size() - 1);
+        System.out.println(initialRegisterA);
     }
-
-    // 2. part
-    List<Integer> expectedOutput = List.of(2,4,1,6,7,5,4,6,1,4,5,5,0,3,3,0);
-    long initialRegisterA = solvePart2(expectedOutput, 0, expectedOutput.size() - 1);
-
-    // Hardcoded input algorithm :(
-    long A = initialRegisterA;
-
-    List<Long> output = new ArrayList<>();
-    while (A != 0) {
-//        // 2,4,
-//        B = A % 8;
-//        // 1,6,
-//        B ^= 6;
-//        // 7,5,
-//        C = A / powerN(2, B);
-//        // 4,6,
-//        B ^= C;
-//        // 1,4,
-//        B ^= 4;
-//        // 5,5,
-//        output.add(String.valueOf(B % 8));
-//        // 0,3,
-//        A /= 8;
-//        // 3,0
-
-        // 2,4,1,6,7,5,4,6,1,4,
-        long o = (((A % 8) ^ 6) ^ (A / powerN(2, (A % 8) ^ 6)) ^ 4) % 8;
-
-        // 5,5,
-        output.add(o);
-        // 0,3,
-        A /= 8;
-        // 3,0
-    }
-
-    System.out.println(expectedOutput.stream()
-            .map(Object::toString)
-            .collect(Collectors.joining(",")));
-    System.out.println(output.stream()
-            .map(Object::toString)
-            .collect(Collectors.joining(",")));
-    System.out.println(initialRegisterA);
 }
 
-
-long solvePart2(List<Integer> expectedOutput, long A, int oi) {
+long solvePart2Hardcoded(List<Integer> expectedOutput, long A, int oi) {
     if (oi < 0) {
         return A; // solution found
     }
     int o = expectedOutput.get(oi);
 
-    for (long i = 0; i < 8; i++) {
-        long A2 = A * 8 + i;
-        if (((((A2 % 8) ^ 6) ^ (A2 / powerN(2, (A2 % 8) ^ 6)) ^ 4) % 8) == o && A2 / 8 == A) {
-            long result = solvePart2(expectedOutput, A2, oi - 1);
+    for (long d = 0; d < 8; d++) {
+        long A2 = A << 3 | d; // A * 8 + d;
+        if (((((A2 % 8) ^ 6) ^ (A2 / powerN(2, (A2 % 8) ^ 6)) ^ 4) % 8) == o) {
+            long result = solvePart2Hardcoded(expectedOutput, A2, oi - 1);
             if (result != -1) {
                 return result;
+            }
+        }
+    }
+
+    return -1; // no solution found
+}
+
+long solvePart2(List<Integer> program, List<Integer> expectedOutput, long A0, int oi) {
+    if (oi < 0) {
+        return A0; // solution found
+    }
+    int o = expectedOutput.get(oi);
+
+    for (long d = 0; d < 8; d++) {
+        long A = A0 << 3 | d;
+        long B = 0;
+        long C = 0;
+
+        int i = 0;
+        prog:
+        while (i <= program.size() - 2) {
+            int op = program.get(i);
+            long lit = program.get(i+1);
+
+            long combo = lit;
+            if (lit == 4) {
+                combo = A;
+            } else if (lit == 5) {
+                combo = B;
+            } else if (lit == 6) {
+                combo = C;
+            }
+
+            switch (op) {
+                case 0: // adv
+                    A /= powerN(2, combo);
+                    i += 2;
+                    break;
+                case 1: // bxl
+                    B ^= lit;
+                    i += 2;
+                    break;
+                case 2: // bst
+                    B = combo % 8;
+                    i += 2;
+                    break;
+                case 3: // jnz
+                    if (A != 0) {
+                        i = (int) lit;
+                    } else {
+                        i += 2;
+                    }
+                    break;
+                case 4: // bxc
+                    B ^= C;
+                    i += 2;
+                    break;
+
+                case 5: // out
+                    long out = combo % 8;
+                    if (out == o) {
+                        long result = solvePart2(program, expectedOutput, A0 << 3 | d, oi - 1);
+                        if (result != -1) {
+                            return result;
+                        }
+                    }
+                    break prog; // stop at first output
+                case 6: // bdv
+                    B = A / powerN(2, combo);
+                    i += 2;
+                    break;
+
+                case 7: // cdv
+                    C = A / powerN(2, combo);
+                    i += 2;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid op: " + op);
             }
         }
     }
